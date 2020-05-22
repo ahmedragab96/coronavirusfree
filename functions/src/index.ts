6754ed30a3bb05dbf4185d3bd64317d462da7fe8
ahmedragab96 <hamadar1996@gmail.com>
2020-05-23 01:44:40 +0200

    add firestore data using cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,20 @@
 import * as functions from 'firebase-functions';
+// const admin = require('firebase-admin');
+import * as admin from 'firebase-admin';
+admin.initializeApp(functions.config().firebase);
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest(async (request, response) => {
+  await createPost();
+  response.send("Hello from Firebase!");
+});
+
+const createPost = (async () => {
+  await admin.firestore().collection('posts').add({
+    title: 'new POst',
+    description: 'decription post',
+    link: 'https://www.google.com',
+  })
+});
